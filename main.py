1b3120eeda3ca49d7dbed5b5515f154cc08f805b
Jacob Mason <jakemason5@unm.edu>
2021-08-17 15:10:41 -0600

    Got the system working accepting new users and logging into the system based on stored accounts. Attempted to use getpass for login but would not work with system.

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
-import getpass
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print('Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    choice = input("Choose Login or New User")
+    choice = input("Choose Login or New User: ")  # prompt user choice
 
+    # if user has login credentials
     if choice == "Login":
-        d = {}
+        d = {} # creating database
+        # opening text file and reading saved login information
         with open('passwords.txt') as f:
             for line in f:
-                (key, val) = line.split()
-                d['key'] = val
+                key, val = line.split()
+                d[key] = val
 
-        username = input("Enter Username")
-        password = getpass.getpass("Enter Password")
+        # prompting user credentials
+        print("Enter Login Information")
+        username = input("Enter Username: ")
+        password = input("Enter Password: ")
+        # Testing the dictionary for username and password combo
         for i in d.keys():
             if username == i:
                 while password != d.get(i):
-                    password = getpass.getpass("Password incorrect")
+                    password = input("Password incorrect")
                 break
         print("Verified User")
 
+    # If user needs to create credentials
     else:
-        username = input("Enter Username")
-        password = input("Enter Password")
-        lines = [username, password]
+        print("Creating New User")
+        username = input("Enter Username: ")
+        password = input("Enter Password: ")
+        lines = [username, password] # storing new username and password
+        # adding the new username and password to text file database
         with open('passwords.txt', 'a') as f:
             for line in lines:
                 f.write(line)
                 f.write(" ")
             f.write('\n')
 
+        # Temporary until making the system continuous
         print("Run the program again to login to the system")
 
